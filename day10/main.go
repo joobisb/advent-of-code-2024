6e975c8be71960fd1dfb5f1c01538b82933a9cc7
joobisb <joobisb@gmail.com>
2024-12-12 13:53:15 +0530

    aoc24: day10 part1 solution

--- day10/main.go
+++ day10/main.go
@@ -12,19 +12,16 @@ var score int
 func main() {
 
 	data, _ := readFile()
-	fmt.Println("2D Array:")
-	for _, row := range data {
-		fmt.Println(row)
-	}
+
 	rowLen := len(data)
 	colLen := len(data[0])
 
 	for i, row := range data {
 		for j, _ := range row {
 			if data[i][j] == "0" {
-				tempVisited := make(map[int]int)
+				//using this to mark already visited 9s
+				tempVisited := make(map[string]bool)
 				checkScores(tempVisited, rowLen, colLen, 0, i, j, data)
-				fmt.Println("check")
 			}
 		}
 	}
@@ -32,15 +29,12 @@ func main() {
 	fmt.Printf("score: %d\n", score)
 }
 
-func checkScores(tempVisited map[int]int, rowLen, colLen, prev, i, j int, data [][]string) {
+func checkScores(tempVisited map[string]bool, rowLen, colLen, prev, i, j int, data [][]string) {
 	if prev == 9 {
-		if tempVisited[i] != j {
+		if !tempVisited[strconv.Itoa(i)+strconv.Itoa(j)] {
 			score++
 		}
-		if tempVisited[i] == j {
-			fmt.Println("here again")
-		}
-		tempVisited[i] = j
+		tempVisited[strconv.Itoa(i)+strconv.Itoa(j)] = true
 		return
 	}
 	if i < 0 || j < 0 || i > rowLen || j > colLen {
@@ -76,7 +70,6 @@ func checkScores(tempVisited map[int]int, rowLen, colLen, prev, i, j int, data [
 			checkScores(tempVisited, rowLen, colLen, prev+1, i-1, j, data)
 		}
 	}
-	return
 
 }
 
@@ -88,21 +81,17 @@ func readFile() ([][]string, error) {
 	}
 	defer file.Close()
 
-	// Slice to hold the 2D array as strings
 	var array [][]string
 
-	// Create a scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		// Treat each digit in the row as an integer
 		var row []string
 		for _, char := range line {
 			row = append(row, string(char))
 		}
 
-		// Append the row to the 2D array
 		array = append(array, row)
 	}
 
